13f8bf6624f594c8280cfff0431edf55d39e89dc
Callum Lees <hi@c-l.dev>
2024-11-12 18:47:29 +0000

    Used NextResponse in subsribe route

--- app/api/subscribe/route.ts
+++ app/api/subscribe/route.ts
@@ -1,6 +1,5 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
 import mailchimp from '@mailchimp/mailchimp_marketing';
-import { NextRequest } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
 
 // Initialize Mailchimp client
 const MAILCHIMP_API_KEY = process.env.MAILCHIMP_API_KEY;
@@ -15,12 +14,12 @@ mailchimp.setConfig({
     server: MAILCHIMP_API_SERVER,
 });
 
-export async function POST(req: NextRequest, res: NextApiResponse) {
+export async function POST(req: NextRequest) {
     const body = await req.json();
     const { email_address } = body;
 
     if (!email_address || email_address == "" || email_address.trim() == "" || email_address == null) {
-        return new Response(
+        return new NextResponse(
             JSON.stringify({ error: 'Email is required' }), {status: 400},
         );
     }
@@ -33,7 +32,7 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
             );
             
             if (existingMember) {
-                return new Response(
+                return new NextResponse(
                     JSON.stringify({ error: 'This email is already subscribed' }),
                     { status: 400 }
                 );
@@ -42,7 +41,7 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
             // If the error is not a 404, throw it
             if (error.status !== 404) {
                 console.error("Error checking existing email:", error);
-                return new Response(
+                return new NextResponse(
                     JSON.stringify({ error: 'An error occurred while checking email' }),
                     { status: 500 }
                 )
@@ -55,13 +54,13 @@ export async function POST(req: NextRequest, res: NextApiResponse) {
         });
 
         if (response.status === 'subscribed') {
-            return new Response(
+            return new NextResponse(
                 JSON.stringify({ message: 'Subscribed successfully', response }),
                 { status: 200 }
             );
         }
     } catch (error) {
-        return new Response(
+        return new NextResponse(
             JSON.stringify({ error: 'Failed to subscribe' }),
             { status: 500 }
         );
