84f750f7b1f17a4f4f0ebe0344bfab5446190eb7
Callum Lees <hi@c-l.dev>
2024-11-12 19:18:57 +0000

    Testing cdn images for loading speed

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    domains: ['images.unsplash.com'], // Allow images from Unsplash
+    deviceSizes: [320, 420, 768, 1024, 1200], // Default device sizes for responsive images
+    imageSizes: [16, 32, 48, 64, 96], // Define custom image sizes
+    formats: ['image/avif', 'image/webp'], // Enable modern formats like WebP and AVIF
+  },
 };
 
 export default nextConfig;
