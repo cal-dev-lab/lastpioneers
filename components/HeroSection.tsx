a82d3541ebbe1c602baaea17eb984dfde5c4df1d
Callum Lees <hi@c-l.dev>
2024-11-12 18:52:36 +0000

    Added priority and npm i sharp

--- components/HeroSection.tsx
+++ components/HeroSection.tsx
@@ -4,7 +4,7 @@ import HeroImg from "../public/images/hero.png"
 export default function HeroSection() {
     return (
         <section id="home" className="relative">
-            <Image src={HeroImg} alt="hero" className="w-full h-screen object-cover" />
+            <Image src={HeroImg} alt="hero" className="w-full h-screen object-cover" priority />
             <div className="hidden md:block space-y-4 absolute left-6 bottom-10 md:bottom-10 md:left-10">
                 <p className="text-white text-4xl md:text-6xl lg:text-8xl">THE LAST</p>
                 <p className="text-white text-4xl md:text-6xl lg:text-8xl">PIONEERS</p>
--- components/ListenSection.tsx
+++ components/ListenSection.tsx
@@ -8,7 +8,7 @@ export default function ListenSection() {
     return (
         <section className="w-full relative h-screen">
             <div className="grid grid-cols-1 md:grid-cols-2 items-center h-screen px-6 md:px-10 py-20">
-                <Image src={albumImg} alt="listen section"className="w-full object-cover" />
+                <Image src={albumImg} alt="listen section"className="w-full object-cover" priority />
 
                 <section className="space-y-6 text-white">
                     <h2 className="text-3xl">OUT NOW</h2>
