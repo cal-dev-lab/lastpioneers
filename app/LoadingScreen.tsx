d39c511ee919b0a768de39a56832e7c8fd6fab12
Callum Lees <hi@c-l.dev>
2024-11-13 10:15:56 +0000

    Added typing animation

--- app/LoadingScreen.tsx
+++ app/LoadingScreen.tsx
@@ -1,16 +1,19 @@
+import TypingAnimation from "@/components/ui/typing-animation";
+
 export default function LoadingScreen() {
     return (
         <section
             className="bg-black z-[999] h-screen w-full flex flex-col items-center justify-center"
         >
             <div className="block space-y-2 md:space-y-6">
-                <p className="text-white font-hollow text-5xl md:text-6xl lg:text-9xl">THE LAST</p>
-                <p className="text-white font-hollow text-5xl md:text-6xl lg:text-9xl">PIONEERS</p>
+                <h1 className="text-white font-hollow text-5xl md:text-6xl lg:text-9xl">THE LAST</h1>
+                <h1 className="text-white font-hollow text-5xl md:text-6xl lg:text-9xl">PIONEERS</h1>
             </div>
 
-            <p className="text-white tracking-widest">
-                loading...
-            </p>
+            <TypingAnimation
+                className="text-white text-xl tracking-widest"
+                text="loading..."
+            />
         </section>
     )
 }
