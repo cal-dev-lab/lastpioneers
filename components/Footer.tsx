a8af90180a4d74b7e33cf91f235db8326b80e6a1
Callum Lees <hi@c-l.dev>
2024-11-13 17:02:08 +0000

    Updated font size in footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,7 +3,7 @@ import SubscribeForm from "@/app/SubscribeForm";
 export default function Footer() {
     return (
         <section className="w-full h-20 bg-black flex items-center px-6 md:px-10 justify-between">
-            <div className="text-white text-3xl flex items-center gap-4">
+            <div className="text-white text-xl md:text-3xl flex items-center gap-4">
                 <h3>THE LAST PIONEERS</h3>
                 <p className="text-sm">Copyright © 2024 The Last Pioneers</p>
             </div>
