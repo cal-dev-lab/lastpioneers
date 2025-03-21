c89b0e2a2b2f2ce441d7b3e7d34c7f898ced4fc2
cal-dev-lab <dev.cal.lees@gmail.com>
2025-03-21 14:13:56 +0000

    Updated bg colour of shop now banner

--- components/ShopNowBanner.tsx
+++ components/ShopNowBanner.tsx
@@ -3,7 +3,7 @@ import { Button } from "./ui/button";
 
 export default function ShopNowBanner() {
   return (
-    <section className="h-20 flex items-center">
+    <section className="h-20 bg-white flex items-center">
       <div className="container mx-auto px-4">
         <div className="flex items-center justify-between">
           <p>NEW MERCH AVAILABLE NOW</p>
